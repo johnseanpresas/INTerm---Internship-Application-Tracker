4b8320924c0c1b36ecaa7c8be67c2bdc5d8341e2
johnseanpresas <johnseanpresas@gmail.com>
2026-09-14 23:21:38 +0800

    feat: setup Taillwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <div>
-      <h1>INTerm</h1>
-      <p>Internship Application Tracker</p>
+    <div className="min-h-screen bg-slate-100 p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        INTerm
+      </h1>
+
+      <p className="mt-2 text-slate-600">
+        Internship Application Tracker
+      </p>
     </div>
   )
 }
